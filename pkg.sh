3006f92fa0a7669092d1e3e3218526d8d78e7584
root <root@localhost.localdomain>
2022-06-17 03:42:25 +0000

    Added memory check

--- pkg.sh
+++ pkg.sh
@@ -11,13 +11,23 @@ if [ ${USER} != root ]
 then
     echo "You need root access to run this script."
 else 
-
+   
     echo "Installation in progress ....."
-    echo "Successfully installed!"
-
+    echo "checking memory memory size. Please, wait !"
+    free -m 
+    echo "checking the number of CPU. Please, wait !"
+    nproc
+    echo "Checking the system hard drives. Please, wait !"
+    lsblk
+    echo "Checking installed OS ...."
+    cat /etc/os-release
+    echo "Installing finger...."
     yum install finger -y
+    echo "Installing curl...."
     yum install curl -y
+    echo "Installing zip...."
     yum install zip -y
+    echo "Installing vim...."
     yum install vim -y
 fi
 exit 1
