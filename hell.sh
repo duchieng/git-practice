8e60f595777c4c31ca42c0585fdb12ff9c91f10b
David <udngira2004@yahoo.com>
2022-06-17 21:15:40 -0400

    Added new lines

--- hell.sh
+++ hell.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+############################################################
+# Program: hell.sh                                         #
+# Description: Install Programs                            #
+# Author: David N Uchieng                                  #
+############################################################
+
 echo "Hello World!"
 echo "This is my first script program."
+
+echo "Checking for memory size"
+free -m
+echo "Installing vim - wait ......"
+yun insall vim -y
