3dcbc158b699acd48a856ef0abc67a48a52ec2b5
root <root@localhost.localdomain>
2022-06-18 02:48:26 +0000

    DY2022

--- pkg.sh
+++ pkg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ###########################################################################
 # Script      : pkg.sh                                                    #
-# Description : This script installs finger, curl, zip and vim packages.  #
+# Description : Inventory check.                                          #
 # Date        :                                                           #
-# 06/09/2022  : Initial coding: David N Uchieng                           #
+# 06/16/2022  : Initial coding: David N Uchieng                           #
 #                                                                         #
 ###########################################################################
 
@@ -12,22 +12,15 @@ then
     echo "You need root access to run this script."
 else 
    
-    echo "Installation in progress ....."
-    echo "checking memory memory size. Please, wait !"
+    echo "Checking system inventory ....."
+    echo "Checking memory memory size. Please, wait !"
     free -m 
-    echo "checking the number of CPU. Please, wait !"
+    echo "Checking the number of CPU. Please, wait !"
     nproc
     echo "Checking the system hard drives. Please, wait !"
     lsblk
     echo "Checking installed OS ...."
     cat /etc/os-release
-    echo "Installing finger...."
-    yum install finger -y
-    echo "Installing curl...."
-    yum install curl -y
-    echo "Installing zip...."
-    yum install zip -y
-    echo "Installing vim...."
-    yum install vim -y
+    
 fi
 exit 1
