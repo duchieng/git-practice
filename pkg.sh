05381329a7c953e2a07aa30a2a145db141363eec
David <udngira2004@yahoo.com>
2022-06-22 15:43:38 -0400

    Script Practice

--- pkg.sh
+++ pkg.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-###########################################################################
-# Script      : pkg.sh                                                    #
-# Description : This script installs finger, curl, zip and vim packages.  #
-# Date        :                                                           #
-# 06/09/2022  : Initial coding: David N Uchieng                           #
-#                                                                         #
-###########################################################################
 
-if [ ${USER} != root ]
-then
-    echo "You need root access to run this script."
-else 
+    #Author : Utrains
+    #Date : 01-Nov-2021
 
-    echo "Installation in progress ....."
-    echo "Successfully installed!"
+## ---------- script that Install some packages in Linux -----------------
 
-    yum install finger -y
-    yum install curl -y
-    yum install zip -y
-    yum install vim -y
-fi
-exit 1
+yum install finger -y 
+yum install curl -y 
+yum install zip -y 
+yum install vim -y 
